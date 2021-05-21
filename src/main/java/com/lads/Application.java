29fade95b5bd9bf761f7660f2488c153ab31576b
GooseAye <84078154+GooseAye@users.noreply.github.com>
2021-05-21 21:19:02 +0100

    Add files via upload
    Added a comment

--- src/main/java/com/lads/Application.java
+++ src/main/java/com/lads/Application.java
@@ -5,7 +5,7 @@ import com.lads.view.GuiChoice;
 public class Application {
     public static void main(String[] args) {
 
-    	// opens the first GUI
+    	//Opens the first GUI
         GuiChoice gui = new GuiChoice();
         gui.setVisible(true);
     }
